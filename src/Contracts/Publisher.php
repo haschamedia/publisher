e6a04980cb438e3c184b6b37720f13beeb3f7b5e
Fariz Huzairi <farizhuzairi@gmail.com>
2024-07-29 07:52:44 +0700

    source code

--- src/Contracts/Publisher.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Publisher\Contracts;
-
-abstract class Publisher
-{}
--- src/PublisherServiceProvider.php
+++ src/PublisherServiceProvider.php
@@ -1,16 +1,30 @@
 <?php
 
-namespace HaschaDev;
+namespace Publisher;
 
 use Illuminate\Support\ServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider
+class PublisherServiceProvider extends ServiceProvider
 {
     public function register(): void
     {}
     
     public function boot(): void
     {
-        //
+        /**
+         * config to publish
+         * 
+         */
+        $this->publishes([
+            __DIR__.'/../config/publisher.php' => config_path('publisher.php'),
+        ], 'publisher-config');
+
+        /**
+         * config to join
+         * 
+         */
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/publisher.php', 'publisher'
+        );
     }
 }
